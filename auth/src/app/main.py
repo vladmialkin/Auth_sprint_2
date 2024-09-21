c59f1e4d9f2861ac43814f39b91baccb0a12bfb7
vladmialkin <myalkin.vlad@yandex.ru>
2024-09-21 11:58:45 +0300

    feat: Добавление jaeger; Добавление ограничения запросов к серверу auth через nginx.

--- auth/src/app/main.py
+++ auth/src/app/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
@@ -10,6 +10,12 @@ from app.db import postgresql, redis
 from app.settings.api import settings as api_settings
 from app.settings.postgresql import settings as postgresql_settings
 from app.settings.redis import settings as redis_settings
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 
 
 @asynccontextmanager
@@ -27,6 +33,22 @@ async def lifespan(_: FastAPI):
     await postgresql.async_engine.dispose()
 
 
+def configure_tracer() -> None:
+    trace.set_tracer_provider(TracerProvider())
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(
+            JaegerExporter(
+                agent_host_name='localhost',
+                agent_port=6831,
+            )
+        )
+    )
+    # Чтобы видеть трейсы в консоли
+    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
+
+
+configure_tracer()
+
 app = FastAPI(
     title=api_settings.TITLE,
     openapi_url=api_settings.OPENAPI_URL,
@@ -35,5 +57,17 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
+
+FastAPIInstrumentor.instrument_app(app)
+
+
+@app.middleware('http')
+async def before_request(request: Request, call_next):
+    response = await call_next(request)
+    request_id = request.headers.get('X-Request-Id')
+    if not request_id:
+        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': 'X-Request-Id is required'})
+    return response
+
 
 app.include_router(v1_router)
