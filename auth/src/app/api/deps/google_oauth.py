927b9c1866c9b474e48d66624ea36018a132d299
Nikolay Shvets <kolya.shvets20@gmail.com>
2024-09-24 02:08:39 +0300

    feat: добавляет проверку socail для oauth

--- auth/src/app/api/deps/google_oauth.py
+++ auth/src/app/api/deps/google_oauth.py
@@ -1,8 +1,8 @@
 from httpx_oauth.clients.google import GoogleOAuth2
 
-from app.settings.google_oauth import settings as google_oauth_settings
+from auth.src.app.settings.oauth import settings as oauth_settings
 
 google_oauth2_client = GoogleOAuth2(
-    google_oauth_settings.GOOGLE_OAUTH_CLIENT_ID,
-    google_oauth_settings.GOOGLE_OAUTH_CLIENT_SECRET,
+    oauth_settings.GOOGLE_OAUTH_CLIENT_ID,
+    oauth_settings.GOOGLE_OAUTH_CLIENT_SECRET,
 )
